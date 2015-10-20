54a4c5b90926563a1e8a9341c63e883db1fd17b9
VisualPi <gintakun2@yahoo.fr>
2015-10-20 16:08:07 +0200

    Commit projet vide unreal

---
+++ ProjetAnnuel/ProjetAnnuel/Source/ProjetAnnuel/ProjetAnnuelGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjetAnnuel.h"
+#include "ProjetAnnuelGameMode.h"
+
+
+
+
---
+++ ProjetAnnuel/ProjetAnnuel/Source/ProjetAnnuel/ProjetAnnuelGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProjetAnnuelGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJETANNUEL_API AProjetAnnuelGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
