900972f0d5812f5c678f18601291f88cd42ad09f
VisualPi <gintakun2@yahoo.fr>
2016-04-11 20:51:33 +0200

    Commit de la scene de base

---
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/Scripts/CSV/CSVParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class csvPareser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		CSVReader.DebugOutputGrid(CSVReader.SplitCsvGrid(@"C:\Users\Jonathan\Desktop\sample.csv"));
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/Scripts/Case.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ECaseType { DEPART, FIN, BLUE, RED, GREEN, YELLOW }
+
+public class Case : MonoBehaviour
+{
+	[SerializeField]
+	private Case            _previousCase;
+	[SerializeField]
+	private Case            _nextCase;
+	[SerializeField]
+	private ECaseType       _caseType;
+	[SerializeField]
+	private MeshRenderer    _meshRenderer;
+	[SerializeField]
+	private Transform       _transform;
+
+	private int _nbPlayerOnCase;
+
+
+	//Init la couleur de chaque case en fonction de leur types. Set le nombre de joueur a 0 sauf pour la case de départ
+	void Start()
+	{
+		switch( _caseType )
+		{
+		case ECaseType.DEPART:
+			_meshRenderer.material = Resources.Load("Materials/Case/Purple", typeof(Material)) as Material;
+			break;
+		case ECaseType.FIN:
+			_meshRenderer.material = Resources.Load("Materials/Case/Black", typeof(Material)) as Material;
+			break;
+		case ECaseType.BLUE:
+			_meshRenderer.material = Resources.Load("Materials/Case/Blue", typeof(Material)) as Material;
+			break;
+		case ECaseType.RED:
+			_meshRenderer.material = Resources.Load("Materials/Case/Red", typeof(Material)) as Material;
+			break;
+		case ECaseType.GREEN:
+			_meshRenderer.material = Resources.Load("Materials/Case/Green", typeof(Material)) as Material;
+			break;
+		case ECaseType.YELLOW:
+			_meshRenderer.material = Resources.Load("Materials/Case/Yellow", typeof(Material)) as Material;
+			break;
+		}
+		if( _caseType != ECaseType.DEPART )
+			_nbPlayerOnCase = 0;
+		else
+			_nbPlayerOnCase = 4;
+	}
+
+	void Update()
+	{
+
+	}
+
+	#region Getter/Setter
+	public int GetNbPlayerOnCase()
+	{
+		return _nbPlayerOnCase;
+	}
+	public void SetNbPlayerOnCase( int value )
+	{
+		_nbPlayerOnCase = value;
+	}
+	public Case GetPreviousCase()
+	{
+		return _previousCase;
+	}
+	public void SetPreviousCase( Case value )
+	{
+		_previousCase = value;
+	}
+	public Case GetNextCase()
+	{
+		return _nextCase;
+	}
+	public void SetNextCase( Case value )
+	{
+		_nextCase = value;
+	}
+	#endregion
+
+}
