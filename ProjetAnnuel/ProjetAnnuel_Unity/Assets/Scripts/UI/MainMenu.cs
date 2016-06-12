1b9530e1816408f04717d5aedae917947ce0c065
VisualPi <gintakun2@yahoo.fr>
2016-06-12 17:19:25 +0200

    add first ui scene

---
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public Transform MainMenuPanel;
+    public Transform StartGamePanel;
+
+    public string playerBlueName;
+    private string playerRedName;
+    private string playerGreenName;
+    private string playerYellowName;
+
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+    public void OnClickStart()
+    {
+        MainMenuPanel.gameObject.SetActive(false);
+        StartGamePanel.gameObject.SetActive(true);
+    }
+    public void OnClickReturnFromStartPanel()
+    {
+        StartGamePanel.gameObject.SetActive(false);
+        MainMenuPanel.gameObject.SetActive(true);
+    }
+
+    public void OnBlueNameEdit(string name)
+    {
+        playerBlueName = name;
+    }
+    public void OnRedNameEdit(string name)
+    {
+        playerRedName = name;
+    }
+    public void OnGreenNameEdit(string name)
+    {
+        playerGreenName = name;
+    }
+    public void OnYellowNameEdit(string name)
+    {
+        playerYellowName = name;
+    }
+
+}
