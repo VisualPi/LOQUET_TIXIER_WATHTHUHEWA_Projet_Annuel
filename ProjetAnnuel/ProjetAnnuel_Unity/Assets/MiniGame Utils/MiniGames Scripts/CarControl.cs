344d41095e565798782b4779b7026f5fe1540c42
LeRyokan <tixier.lucas@gmail.com>
2016-06-27 22:14:02 +0200

    Adding some gameplay

--- ProjetAnnuel/ProjetAnnuel_Unity/Assets/MiniGame Utils/MiniGames Scripts/CarControl.cs
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/MiniGame Utils/MiniGames Scripts/CarControl.cs
@@ -2,14 +2,40 @@
 using System.Collections;
 
 public class CarControl : MonoBehaviour {
-
+    Rigidbody carRigid;
+    Transform carTransform;
 	// Use this for initialization
 	void Start () {
-	
+        carRigid = GetComponent<Rigidbody>();
+        carTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+        if(Input.GetKey(KeyCode.Z))
+        {
+            carRigid.AddForce(carTransform.forward * 15,ForceMode.Acceleration);
+        }
+        
+        if(Input.GetKey(KeyCode.D))
+        {
+            carTransform.Rotate(new Vector3(0, 5, 0));
+        }
+        
+        if(Input.GetKey(KeyCode.Q))
+        {
+            carTransform.Rotate(new Vector3(0, -5, 0));
+            
+        }
+        
+        if(Input.GetKey(KeyCode.S))
+        {
+            carRigid.AddForce(-carTransform.forward * 10   , ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            carRigid.velocity = carRigid.velocity * 0.7f;
+        }
 	}
 }
