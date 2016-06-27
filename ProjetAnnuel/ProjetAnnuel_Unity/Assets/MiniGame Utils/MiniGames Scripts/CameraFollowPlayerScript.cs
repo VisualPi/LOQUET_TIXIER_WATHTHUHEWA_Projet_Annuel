21de2304fc02d090789d897d32a1c9d01eb80694
LeRyokan <tixier.lucas@gmail.com>
2016-06-27 22:24:08 +0200

    Camera pour la course

---
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/MiniGame Utils/MiniGames Scripts/CameraFollowPlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayerScript : MonoBehaviour 
+{
+    [SerializeField]
+    Transform _cameraTransform;
+
+    [SerializeField]
+    Transform _targetTransform;
+
+    [SerializeField]
+    float _distanceX;
+    [SerializeField]
+    float _distanceY;
+    [SerializeField]
+    float _distanceZ;
+
+    [SerializeField]
+    float _rotationX;
+    [SerializeField]
+    float _rotationY;
+    [SerializeField]
+    float _rotationZ;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _cameraTransform.Rotate(new Vector3(_rotationX, _rotationY, _rotationZ));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float x = _targetTransform.position.x + _distanceX;
+        float y = _targetTransform.position.y + _distanceZ;
+        float z = _targetTransform.position.z + _distanceY;
+        _cameraTransform.position = new Vector3(x, y, z);
+        _cameraTransform.LookAt(_targetTransform);
+        
+	}
+}
