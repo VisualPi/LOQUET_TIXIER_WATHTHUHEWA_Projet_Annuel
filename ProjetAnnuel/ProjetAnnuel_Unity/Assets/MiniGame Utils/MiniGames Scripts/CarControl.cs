12e61a3425f4a66564013d6eeb42740cc984e3fb
LeRyokan <tixier.lucas@gmail.com>
2016-07-01 19:06:08 +0200

    Toutes les ressources pour le mini jeu de la course

--- ProjetAnnuel/ProjetAnnuel_Unity/Assets/MiniGame Utils/MiniGames Scripts/CarControl.cs
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/MiniGame Utils/MiniGames Scripts/CarControl.cs
@@ -4,38 +4,96 @@ using System.Collections;
 public class CarControl : MonoBehaviour {
     Rigidbody carRigid;
     Transform carTransform;
+    public int playerID;
+    public int lapsDone;
+    public bool checkpointReach;
+    public int position;
+
 	// Use this for initialization
 	void Start () {
+
+        
         carRigid = GetComponent<Rigidbody>();
         carTransform = GetComponent<Transform>();
-	}
+        lapsDone = 0;
+        checkpointReach = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-        if(Input.GetKey(KeyCode.Z))
+        if (playerID == 1)
         {
-            carRigid.AddForce(carTransform.forward * 15,ForceMode.Acceleration);
+            //Player 1
+            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Joystick1Button5))
+            {
+                carRigid.AddForce(carTransform.forward * 12, ForceMode.Acceleration);
+            }
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick1Button6))
+            {
+                carRigid.velocity = carRigid.velocity * 0.9f;
+            }
+
+            if (Input.GetAxis("Horizontal") == -1 || Input.GetAxis("Horizontal") == 1)
+            {
+                carTransform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * 2, 0));
+            }
         }
-        
-        if(Input.GetKey(KeyCode.D))
+        if (playerID == 2)
         {
-            carTransform.Rotate(new Vector3(0, 5, 0));
+            //Player 2        
+            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Joystick2Button5))
+            {
+                carRigid.AddForce(carTransform.forward * 12, ForceMode.Acceleration);
+            }
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick2Button6))
+            {
+                carRigid.velocity = carRigid.velocity * 0.9f;
+            }
+
+            if (Input.GetAxis("Horizontal2") == -1 || Input.GetAxis("Horizontal2") == 1)
+            {
+                carTransform.Rotate(new Vector3(0, Input.GetAxis("Horizontal2") * 2, 0));
+            }
         }
-        
-        if(Input.GetKey(KeyCode.Q))
+
+        if (playerID == 3)
         {
-            carTransform.Rotate(new Vector3(0, -5, 0));
-            
+            //Player 3
+            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Joystick3Button5))
+            {
+                carRigid.AddForce(carTransform.forward * 12, ForceMode.Acceleration);
+            }
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick3Button6))
+            {
+                carRigid.velocity = carRigid.velocity * 0.9f;
+            }
+
+            if (Input.GetAxis("Horizontal3") == -1 || Input.GetAxis("Horizontal3") == 1)
+            {
+                carTransform.Rotate(new Vector3(0, Input.GetAxis("Horizontal3") * 2, 0));
+            }
         }
-        
-        if(Input.GetKey(KeyCode.S))
+        if (playerID == 4)
         {
-            carRigid.AddForce(-carTransform.forward * 10   , ForceMode.Acceleration);
+            //Player 4
+            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Joystick4Button5))
+            {
+                carRigid.AddForce(carTransform.forward * 12, ForceMode.Acceleration);
+            }
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Joystick4Button6))
+            {
+                carRigid.velocity = carRigid.velocity * 0.9f;
+            }
+
+            if (Input.GetAxis("Horizontal4") == -1 || Input.GetAxis("Horizontal4") == 1)
+            {
+                carTransform.Rotate(new Vector3(0, Input.GetAxis("Horizontal4") * 2, 0));
+            }
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            carRigid.velocity = carRigid.velocity * 0.7f;
-        }
-	}
+
+
+
+
+
+    }
 }
