2ddad87fc9d18fb9e8669b8939c551a861cfd469
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2016-07-04 17:53:23 +0200

    Commit IA basique

--- ProjetAnnuel/AI/Assets/Scripts/AI/AiScript.cs
+++ ProjetAnnuel/AI/Assets/Scripts/AI/AiScript.cs
@@ -17,6 +17,14 @@ public class AiScript : MonoBehaviour
     [SerializeField]
     float _minDistanceFromCheckPoint;
 
+    [SerializeField]
+    float _angleMaxRange;
+
+    [SerializeField]
+    float _secondsBetweenRandom;
+
+    float _angleApproximation;
+
     List<CheckPointScript> _path;
 
     int _currentCheckPointIndex;
@@ -30,12 +38,13 @@ public class AiScript : MonoBehaviour
             if (_currentCheckPoint != null)
             {
                 Gizmos.color = Color.red;
-                Gizmos.DrawWireSphere(_currentCheckPoint.transform.position, _currentCheckPoint._radius + 0.10f);
+                Gizmos.DrawWireSphere(_currentCheckPoint.transform.position, _currentCheckPoint._radius - 1.00f);
             }
             
             Gizmos.color = Color.yellow;
 
-            Gizmos.DrawRay(transform.position, (transform.forward) * 5);
+            Gizmos.DrawRay(transform.position, (transform.forward) * 3);
+            /*
             Gizmos.DrawRay(transform.position, (transform.forward + -transform.right) * 5);
             Gizmos.DrawRay(transform.position, (transform.forward + transform.right) * 5);
 
@@ -45,27 +54,88 @@ public class AiScript : MonoBehaviour
 
             Gizmos.DrawRay(transform.position, (transform.right)*3);
             Gizmos.DrawRay(transform.position, (-transform.right)*3);
-            
+            */
         }
     }
 
 	// Use this for initialization
 	void Start () 
     {
+        _moveScript._playerMove = false;
+
         _path = _pathScript._path;
 
         if(_path.Count > 0)
             _currentCheckPoint = _path[0];
+
+        StartCoroutine(randomAngle());
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        Debug.Log("Update");
         Move();
 	}
 
+    IEnumerator randomAngle()
+    {
+        while (true)
+        {
+            _angleApproximation = Random.Range(0, _angleMaxRange);
+
+            Debug.Log("random - " + _angleApproximation);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
     void Move()
     {
+        if (_currentCheckPoint != null)
+        {
+            List<KeyCode> iaKeyCodes = new List<KeyCode>();
+            
+            float angle = Vector3.Angle(transform.forward, _currentCheckPoint.transform.position - transform.position);
+           
+            //if ((angle >= 0 && angle <= 45) || (angle >= 135 && angle <= 180))
+            if ((angle >= 0 && angle <= _angleApproximation) || (angle >= (180-_angleApproximation) && angle <= 180))
+            {
+                iaKeyCodes.Add(KeyCode.UpArrow);
+            }
+            else
+            {
+                iaKeyCodes.Add(KeyCode.UpArrow);
+
+                float angleLeft = Vector3.Angle(transform.forward - transform.right, _currentCheckPoint.transform.position - transform.position);
+                float angleRight = Vector3.Angle(transform.forward + transform.right, _currentCheckPoint.transform.position - transform.position);
+
+                if (angleLeft > angleRight)
+                {
+                    iaKeyCodes.Add(KeyCode.RightArrow);
+                }
+                else
+                {
+                    iaKeyCodes.Add(KeyCode.LeftArrow);
+                }
+            }
+
+            PlayerInput pi = new PlayerInput(iaKeyCodes);
+            _moveScript.makeMove(pi);
+            /*                  
+            RaycastHit rayCastHit;
+                
+            if (Physics.Raycast(transform.position, transform.forward, out rayCastHit, 3, 1 << LayerMask.NameToLayer("CheckPoint")))
+            {
+                Debug.Log("hit " + rayCastHit.collider.gameObject.name + " - " + rayCastHit.distance);
+            }
+            else
+            {
+
+            }
+            */
+        }
+
         GetNextPath();
     }
 
