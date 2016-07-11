a73c37d544408a78d5caf452d5eef624040fdeff
LeRyokan <tixier.lucas@gmail.com>
2016-07-11 15:11:19 +0200

    Ajout d'une scene de transition

---
+++ ProjetAnnuel/ProjetAnnuel_Unity/Assets/Scripts/IntroMiniGameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class IntroMiniGameManager : MonoBehaviour {
+
+
+    [SerializeField]
+    Text infoMiniGameText;
+
+	// Use this for initialization
+	void Start () {
+
+        if(PlayerPrefs.GetInt("GAME_MINIGAME")==0) // voiture
+        {
+            infoMiniGameText.text = "JEU DE LA VOITURE ECRIS TA VIE ICI ";
+            StartCoroutine(LaunchMiniGame(3));
+        }
+        else
+        {
+            infoMiniGameText.text = "JEU DES COULEURS ECRIS TA VIE ICI";
+            StartCoroutine(LaunchMiniGame(4));
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator LaunchMiniGame(int value)
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(value);
+    }
+}
